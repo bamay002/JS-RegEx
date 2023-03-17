851302ec51a90f1e0c25258f2b41c00b6afab9e6
Brenda <amaya.brendaa@gmail.com>
2023-03-16 21:07:59 -0700

    final touch

--- regex.js
+++ regex.js
@@ -31,7 +31,10 @@ Expected Output:
 
 */ 
  
-let phoneReg = /write your regex here/
+let phoneReg = /\d{3}-\d{3}-\d{4}/g
+let phoneArr = sampleApp.match(phoneReg)
+console.log(phoneArr)
+
 
 /* 
 
@@ -51,7 +54,9 @@ Expected Output:
 
 */ 
 
-let emailReg = /write your regex here/
+let emailReg = /\w+@[a-zA-Z]+.[a-zA-Z]+/g
+let emailArr = sampleApp.match(emailReg)
+console.log(emailArr)
 
 /* 
 
@@ -67,4 +72,6 @@ Expected Output:
 
 */ 
 
-let statusReg = /write your regex here/
+let statusReg = /status:/i
+let statusArr = sampleApp.search(statusReg)
+console.log(statusArr)
